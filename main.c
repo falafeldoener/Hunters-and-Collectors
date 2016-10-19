22bd54d82ffafb8f2ab11a1a2db8c325396015a2
FalafelDöner <xlukasx@gmx.de>
2016-10-19 20:58:32 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -16,7 +16,23 @@ int main()
     //Declaration main_page selection
     int *piSelection;
 
-    main_page(&piSelection);
+    main_page(piSelection);
+
+    switch(*piSelection)
+    {
+        case 1:
+            printf("1\n");
+        break;
+        case 2:
+            printf("2\n");
+        break;
+        case 3:
+            printf("3\n");
+        break;
+        case 4:
+            printf("4");
+        break;
+    }
 
     return 0;
 }
